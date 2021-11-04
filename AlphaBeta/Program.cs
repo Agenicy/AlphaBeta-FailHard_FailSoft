912f3098c255f1fc0008668c321c19cb7eb2bdc3
Agenicy <agenicy@gmail.com>
2021-11-04 22:33:31 +0800

    AB-SSS / AB-DUAL

--- AlphaBeta/Program.cs
+++ AlphaBeta/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿#define Fail_Soft
+#define AB_SSS_DUAL
+using System;
 using System.Collections.Generic;
 using System.Linq;
+
 
 namespace AlphaBeta
 {
@@ -38,25 +41,36 @@ namespace AlphaBeta
 						NegaMaxNode.AddNode(index, index * 2);
 						NegaMaxNode.AddNode(index, index * 2 + 1);
 					}
+					NegaMaxNode.p[index].index = index;
 				}
 			}
 
-			NegaMaxNode.function = NegaMaxNode.Function.F3;
-
+			NegaMaxNode.function = NegaMaxNode.Function.AB_SSS;
+			float alpha = 30, beta = 40;
 			switch (NegaMaxNode.function)
 			{
 				case NegaMaxNode.Function.BF:
 					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1])}");
 					break;
-				case NegaMaxNode.Function.F:
+				case NegaMaxNode.Function.Alpha_beta:
 					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1])}");
 					break;
 				case NegaMaxNode.Function.F2:
-					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1], float.MinValue, float.MaxValue)}");
+					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1], alpha, beta)}");
 					break;
+#if Fail_Soft
 				case NegaMaxNode.Function.F3:
-					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1], float.MinValue, float.MaxValue)}");
+					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1], alpha, beta)}");
 					break;
+#endif
+#if AB_SSS_DUAL
+				case NegaMaxNode.Function.AB_SSS:
+					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1])}");
+					break;
+				case NegaMaxNode.Function.AB_DUAL:
+					Debug.Log($"Answer: {NegaMaxNode.Run(NegaMaxNode.p[1])}");
+					break;
+#endif
 			}
 
 			Debug.Log($"Run time: {NegaMaxNode.time}");
@@ -72,7 +86,13 @@ namespace AlphaBeta
 	{
 		public enum Function
 		{
-			BF, F, F2, F3
+			BF, Alpha_beta, F2
+#if Fail_Soft
+			, F3
+#endif
+#if AB_SSS_DUAL
+			, AB_SSS, AB_DUAL
+#endif
 		};
 		public static Function function;
 
@@ -82,8 +102,20 @@ namespace AlphaBeta
 		public static List<NegaMaxNode> p;
 
 		public float? value = null;
-		public float? cutoffValue = null;
 
+		public int index;
+
+#if AB_SSS_DUAL
+		/// <summary>
+		/// AB-SSS TT
+		/// </summary>
+		static HashSet<NegaMaxNode> Retrive = new HashSet<NegaMaxNode>();
+
+		/// <summary>
+		/// upper limit and lower limit of node
+		/// </summary>
+		float? f_Plus = null, f_Minus = null;
+#endif
 		#region NotUsed
 		public static float time = 0;
 		public static float timeLimit = float.MaxValue;
@@ -129,12 +161,39 @@ namespace AlphaBeta
 			{
 				case Function.BF:
 					return BF(position);
-				case Function.F:
-					return (float)F(position);
+				case Function.Alpha_beta:
+					return F2(position, float.MinValue, float.MaxValue);
 				case Function.F2:
-					return (float)F2(position, alpha, beta);
+					return F2(position, alpha, beta);
+#if Fail_Soft
 				case Function.F3:
-					return (float)F3(position, alpha, beta);
+					return F3(position, alpha, beta);
+#endif
+#if AB_SSS_DUAL
+				case Function.AB_SSS:
+					Retrive.Clear();
+					beta = 1e8f; // dG
+					int i = 0;
+					do
+					{
+						alpha = beta; // dR = dG
+						Debug.Log($"\nPASS {++i}: dR = {alpha} dG = {beta}");
+						beta = AB(position, alpha - 1, alpha);
+					} while (alpha != beta);
+					return beta;
+
+				case Function.AB_DUAL:
+					Retrive.Clear();
+					alpha = -1e8f; // dG
+					i = 0;
+					do
+					{
+						beta = alpha; // dR = dG
+						Debug.Log($"\nPASS {++i}: dR = {beta}, dG = {alpha}");
+						alpha = AB(position, beta, beta + 1);
+					} while (alpha != beta);
+					return alpha;
+#endif
 			}
 			return 0;
 		}
@@ -164,9 +223,9 @@ namespace AlphaBeta
 		}
 
 		/// <summary>
-		/// Normal Alpha-beta
+		/// NegaMax / Alpha-beta with Fail-hard
 		/// </summary>
-		static float? F(NegaMaxNode position, float? cutoffValue = null)
+		static float F2(NegaMaxNode position, float alpha, float beta)
 		{
 			++time;
 			int d = position.CountChild();
@@ -174,58 +233,37 @@ namespace AlphaBeta
 			if (d == 0
 				|| position.depth == cutoffThreshold
 				|| time > timeLimit)
-				return H(position);
-
-			float m = float.MinValue; // will be the max value
-			for (int i = 0; i < d; i++)
 			{
-				float? t = -F(position.children[i], -position.cutoffValue); // recursive
-
-				// cut off
-				if (cutoffValue != null)
-					if (t > cutoffValue)
-						return null;
-
-				if (t > m)
-				{
-					position.value = position.cutoffValue = m = (float)t;
-				}
-
-			}
-			return m;
-		}
-
-		/// <summary>
-		/// Alpha-beta with Fail-hard
-		/// </summary>
-		static float? F2(NegaMaxNode position, float alpha, float beta)
-		{
-			++time;
-			int d = position.CountChild();
-
-			if (d == 0
-				|| position.depth == cutoffThreshold
-				|| time > timeLimit)
+				Debug.Log($"Node {position.index}: M = {H(position)} Alpha = {alpha}, Beta = {beta}");
 				return H(position);
+			}
 
 			float m = alpha; // will be the max value
 			for (int i = 0; i < d; i++)
 			{
-				float? t = -F2(position.children[i], -beta, -m); // recursive
+				float t = -F2(position.children[i], -beta, -m); // recursive
 
 				if (t > m)
-					position.value = m = (float)t;
+					position.value = m = t;
 
 				if (m >= beta)
+				{
+					if (i < d - 1)
+						Debug.Log($"[Cutoff] Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
+					else
+						Debug.Log($"Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
 					return m;
+				}
 			}
+			Debug.Log($"Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
 			return m;
 		}
 
+#if Fail_Soft
 		/// <summary>
 		/// Alpha-beta with Fail-soft
 		/// </summary>
-		static float? F3(NegaMaxNode position, float alpha, float beta)
+		static float F3(NegaMaxNode position, float alpha, float beta)
 		{
 			++time;
 			int d = position.CountChild();
@@ -233,21 +271,82 @@ namespace AlphaBeta
 			if (d == 0
 				|| position.depth == cutoffThreshold
 				|| time > timeLimit)
+			{
+				Debug.Log($"Node {position.index}: M = {H(position)} Alpha = {alpha}, Beta = {beta}");
 				return H(position);
+			}
 
 			float m = float.MinValue; // will be the max value
 			for (int i = 0; i < d; i++)
 			{
-				float? t = -F3(position.children[i], -beta, -MathF.Max(m, alpha)); // recursive
+				float t = -F3(position.children[i], -beta, -MathF.Max(m, alpha)); // recursive
 
 				if (t > m)
-					position.value = m = (float)t;
+					position.value = m = t;
 
 				if (m >= beta)
+				{
+					if (i < d - 1)
+						Debug.Log($"[Cutoff] Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
+					else
+						Debug.Log($"Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
 					return m;
+				}
 			}
+			Debug.Log($"Node {position.index}: M = {m} Alpha = {alpha}, Beta = {beta}");
 			return m;
 		}
+#endif
+#if AB_SSS_DUAL
+		/// <summary>
+		/// Alpha-beta in AB-SSS* / AB-DUAL
+		/// </summary>
+		static float AB(NegaMaxNode position, float alpha, float beta)
+		{
+			float g = 0;
+
+			/* Check if position is in TT and has been searched to sufﬁcient depth */
+			if (Retrive.Contains(position))
+				if (position.f_Plus <= alpha || ((position.f_Plus == position.f_Minus)&&position.f_Plus != null) )
+				{
+					Debug.Log($"Node {position.index}: fP = {position.f_Plus} Alpha = {alpha}, Beta = {beta}");
+					return (float)position.f_Plus;
+				}
+				else if (position.f_Minus >= beta)
+				{
+					Debug.Log($"Node {position.index}: fM = {position.index} Alpha = {alpha}, Beta = {beta}");
+					return (float)position.f_Minus;
+				}
+
+			int d = position.CountChild();
+			/* Reached the maximum search depth */
+			if (d == 0
+				|| position.depth == cutoffThreshold
+				|| time > timeLimit)
+				position.f_Minus = position.f_Plus = g = (float)position.value;
+			else
+			{
+				g = float.MinValue;
+
+				for (int i = 0; i < d && g < beta; i++)
+				{
+					var c = position.children[i];
+
+					g = MathF.Max(g, -AB(c, -beta, -alpha));
+					alpha = MathF.Max(alpha, g);
+				}
+				if (g < beta)
+					position.f_Plus = g;
+				if (g > alpha)
+					position.f_Minus = g;
+			}
+			Retrive.Add(position);
+
+			Debug.Log($"Node {position.index}: G = {g} Alpha = {alpha}, Beta = {beta}");
+			return g;
+		}
+
+#endif
 	}
 
 	#region Debug.Log
